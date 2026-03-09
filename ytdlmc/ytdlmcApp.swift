dc327bd41f2665e9b35c71a4a25816de288e2636
Ethan <lisen8018@gmail.com>
2026-03-09 22:49:39 +1300

    Initial Commit

---
+++ ytdlmc/ytdlmcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ytdlmcApp.swift
+//  ytdlmc
+//
+//  Created by Eason Smith on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ytdlmcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ytdlmcTests/ytdlmcTests.swift
@@ -0,0 +1,17 @@
+//
+//  ytdlmcTests.swift
+//  ytdlmcTests
+//
+//  Created by Eason Smith on 09/03/2026.
+//
+
+import Testing
+@testable import ytdlmc
+
+struct ytdlmcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
